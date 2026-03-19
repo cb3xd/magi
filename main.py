19fcb76c3cfefe76478233289fe434189b128094
cb3xd <cesar_basco@proton.me>
2026-03-19 20:09:35 +0800

    main.py: Test code for the backend.

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+from typing import Literal, Dict
 from backend import Magi
+from backend.magi import MagiUnit
 
 
 def main():
     magi = Magi()
-    print(magi.units)
+    magi_units: Dict[Literal['Melchior', 'Balthasar', 'Casper'], MagiUnit] = (
+        magi.units
+    )
+    message = '3 Project Proposal Drafts, 1 is a parking meter implementation in a densely populated part of a city. 2. is about protecting local film and medial from cultural homogenization through partnership with schools for increased visibility. and 3. a city-wide digital twin dashboard tracking funding, taxes, projects, etc. Which would you go for?'
+    magi_units['Casper'].add_message(
+        'user',
+        message,
+    )
+    print(f'[Casper] Sending: {message}')
+    magi_units['Casper'].send_messages()
 
 
 if __name__ == '__main__':
